f2d2db756cfcd067405f331e9abeb213998bced1
Boris Fritscher <boris@fritscher.ch>
2021-02-14 16:34:29 +0100

    catch offline

--- src/service-worker.js
+++ src/service-worker.js
@@ -1,5 +1,6 @@
-import { precacheAndRoute } from "workbox-precaching";
+import { precacheAndRoute, matchPrecache } from "workbox-precaching";
 import { googleFontsCache, staticResourceCache } from "workbox-recipes";
+import { setCatchHandler } from "workbox-routing";
 
 addEventListener("message", (event) => {
   if (event.data && event.data.type === "SKIP_WAITING") {
@@ -10,3 +11,13 @@ addEventListener("message", (event) => {
 precacheAndRoute(self.__WB_MANIFEST);
 googleFontsCache();
 staticResourceCache();
+
+// Catch routing errors, like if the user is offline
+setCatchHandler(async ({ event }) => {
+  // Return the precached offline page if a document is being requested
+  if (event.request.destination === "document") {
+    return matchPrecache("./index.html");
+  }
+
+  return Response.error();
+});
